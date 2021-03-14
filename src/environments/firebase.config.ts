201b3ebaae181f3d3e14f22e62a9c12a10b6867d
Pacharaphol <44960790+Kumojima97@users.noreply.github.com>
2021-03-14 22:09:22 +0700

    Update firebase.config.ts

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,12 +1,12 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyAMtRxjCO6TAfRPfXh8GfCfhNY61eD_TLc",
-    authDomain: "database-project-778e9.firebaseapp.com",
-    databaseURL: "https://database-project-778e9.firebaseio.com",
-    projectId: "database-project-778e9",
-    storageBucket: "database-project-778e9.appspot.com",
-    messagingSenderId: "615854187627",
-    appId: "1:615854187627:web:f58bbd6b1143c41723fd2e",
-    measurementId: "G-1D1T6JFKK4"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 export const firebaseFunction = {
